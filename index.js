3fe2788226ed7a05495cb554efd6b179b80ccc28
Misty Islam <134092022+MistyIslam5@users.noreply.github.com>
2023-11-28 23:49:10 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var massage = alert("Hello Bondhugon");
+        console.log(massage);
+        var lastName = "Misty Isalm";
+
+        console.log(lastName)
+
+        var name = "Mizanur Rahamn Misty";
+        document.write(name)
